4a2ede06125c2376748a3ca4e0d29f7bfc062336
Ash Whiting <ash@digitaltactics.co.uk>
2020-02-11 15:04:31 +0000

    adjusted a few settings

--- functions.php
+++ functions.php
@@ -1,26 +1,6 @@
 <?php
 
-namespace arlo;
-
-require_once "vendor/autoload.php";
-
-if(!function_exists('\\arlo\\init')) {
-    function init()
-    {
-        $core = new core();
-        $core->listen();
-
-        $enqueue_scripts = new enqueue_scripts();
-        $enqueue_scripts->listen();
-
-        $menus = new menus();
-        $menus->listen();
-
-        $custom_post_types = new custom_post_types();
-        $custom_post_types->listen();
-    }
-}
-
-add_action( 'wp_loaded', '\\arlo\\init' );
+require_once( 'includes/navwalker.php' ); 
+require_once( 'includes/menus.php' ); 
 
 ?>
