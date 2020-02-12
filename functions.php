d992016a534e645f16e2a1b51e39df17754ee0d5
Ash Whiting <ash@digitaltactics.co.uk>
2020-02-12 11:10:39 +0000

    added classes

--- functions.php
+++ functions.php
@@ -1,6 +1,15 @@
 <?php
+namespace arlo_seed;
 
-require_once( 'includes/navwalker.php' ); 
-require_once( 'includes/menus.php' ); 
+use arlo_seed\utils\file_loader;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+if (!defined('WP_ENV')) {
+    define('WP_ENV', 'production');
+}
+
+// require_once( 'includes/navwalker.php' ); 
+// require_once( 'includes/menus.php' ); 
 
 ?>
